a06970b4aae4ab995316d87348a9cdb1c3604e07
tsukinowaguma <554969905@qq.com>
2017-02-24 15:24:34 +0800

    fix onclick-delay in moblie

--- app/main.jsx
+++ app/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App.jsx';
+import initReactFastclick from 'react-fastclick';
 
+initReactFastclick();
 
 ReactDOM.render(
   <App />,
   document.body.appendChild(document.createElement('div'))
 );
+
